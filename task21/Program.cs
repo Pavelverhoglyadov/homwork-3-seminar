becfb0195092bbb44460dc125ba3b29bf2cbf6d6
pavel <dongod@bk.ru>
2022-12-03 18:59:54 +0400

    перенес в чистовик

---
+++ task21/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Введите координату x1: ");
+Double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y1: ");
+Double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z1: ");
+Double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату x2: ");
+Double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату y2: ");
+Double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите координату z2: ");
+Double z2 = Convert.ToDouble(Console.ReadLine());
+
+double A = x2 - x1;
+double B = y2 - y1;
+double C = z1 - z2;
+
+Console.WriteLine($"Длинна отрезка {A * A + B * B + C * C}");
+
+
+
---
+++ task23/Program.cs
@@ -0,0 +1,15 @@
+﻿
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+int SumNumbers(int N)
+{
+    int mul = 1;
+    for (int i = 1; i <= N; i++)
+        mul = i*i*i;
+    return mul;
+}
+        
+Console.WriteLine("Введите целое число");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($" таблица кубов от 1 до {N} равно {SumNumbers(N)}");
